554f4a6ff4a190a104fcb8a09dbadee18afd58c1
Ron Fox <rfoxkendo@gmail.com>
2025-12-30 10:25:12 -0500

    Issue #1 Define database schema
    Define (at least in comments) the database schema.

---
+++ src/database/mod.rs
@@ -0,0 +1,143 @@
+//! This module provides the database definition
+//! and implementation APIs for the kiln manager.
+//! There are several objects of note:
+//! 
+//! *  Kilns - a kiln is the devicde in which glass projects
+//!    are fired.  The same firing in different kilns can result
+//!    in slightly different results.
+//! *  Firing sequences - these belong to kilns and
+//!    consist of steps, each with a ramp speed and a target temperture.
+//!    note that in this project, temperatures are degrees Fahrenheit and
+//!    ramp speeds are in degrees F/sec.  A special ramp speed AFAP
+//!    means that the ramp is "as fast as  possible".  For ramping down,
+//!    this normally means the heating element is always off during the
+//!    ramp.  For upwards ramps, the heating element is always on for AFAP ramps.
+//! *  Projects are firings done on a kiln to produce a result.  Note that a
+//!    project:
+//!     -  Is associated with one or more firings (firing sequences)
+//!     -  Is associated with zero or more pictures that are intended to
+//!        capture the progress or result of the project.
+//!     -  Since firing sequencdes belong to kilns, a project may use more
+//!        than one kiln.
+//! 
+
+use serde::{Deserialize, Serialize};
+use serde_rusqlite::*;
+/// This structure  represents a Kiln. In Sqlite, it will
+/// be represented as:
+/// ```sql
+/// CREATE TABLE IF NOT EXISTS Kilns (
+///   id           INTEGER PRIMARY KEY AUTOINCREMENT,
+///   name         TEXT        -- name of kiln.
+///   description  TEXT -- describes the kiln.
+/// )
+/// ```
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Kiln {
+    id : u64,
+    name : String,
+    description : String
+}
+
+///  These structures represent a firing sequence
+///  and its steps.
+///  In the database these are:
+/// 
+/// ```sql
+/// CREATE TABLE IF NOT EXISTS Firing_sequences (
+///   id           INTEGER  PRIMARY KEY AUTOINCREMENT,
+///   name        TEXT,  
+///   descripton  TEXT,
+///   kiln_id     INTEGER -- Foreign key into Kilns
+/// )
+/// CREATE TABLE IF NOT EXISTS Firing_step (
+///    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+///    sequence_id INTEGER  -- FK to Firing_sequences
+///    ramp:       INTEGER, -- -1 means AFAP.
+///    target:     INTEGER
+/// )
+/// ```
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct FiringSequence {
+    id : u64,
+    name : String,
+    description : String,
+    kiln_id : u64,
+}
+/// The steps in a firing sequence:
+/// 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct FiringStep {
+    id  :u64,
+    sequence_id : u64,
+    ramp_rate : i32,
+    target_temp : u32
+}
+/// This convenience struct holds a full
+/// kiln program:
+/// 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct KilnProgram {
+    kiln : Kiln,
+    sequence : FiringSequence,
+    steps    : Vec<FiringStep>
+}
+
+/// A project is a set of firing sequences
+/// and an optional set of images.
+/// The tables used to represent this in the
+/// database are:
+/// 
+/// ```sql
+/// CREATE TABLE IF NOT EXISTS Project (
+///    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+///    name        TEXT,
+///    description TEXT
+/// )
+/// CREATE TABLE IF NOT EXISTS Project_firings (
+///    id                 INTEGER PRIMARY KEY AUTOINCREMENT,
+///    project_id         INTEGER -- FK to Project.
+///    firing_sequence_id INTEGER, -- FK to Firing_squences
+///    comment            TEXT  -- maybe why this firing.
+/// )
+/// CREATE TABLE Project_Images (
+///   id         INTEGER PRIMARY KEY AUTOINCREMENT,
+///   project_id INTEGER -- FK to project.
+///   name       TEXT,   -- Original filename e.. final.jpg
+///   caption    TEXT, -- What the picture is.
+///   contents   BLOB -- The image file contents.
+/// )
+/// ```
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Project {
+    id : u64,
+    name : String,
+    description : String
+}
+/// The firing steps associated with a project:
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct ProjectFringSteps {
+    id : u64,
+    project_id : u64,
+    firing_sequence_id : u64,
+    comment : String
+}
+
+/// A picture associated with a project:
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct ProjectImage {
+    id : u64,
+    project_id : u64,
+    nme : String,
+    description : String,
+    contents : Vec<u8>
+}
+
+/// A project fully unpacked from the database:
+/// 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct KilnProject {
+    project : Project,
+    firings : Vec<KilnProgram>,
+    pictures : Vec<ProjectImage>
+}
